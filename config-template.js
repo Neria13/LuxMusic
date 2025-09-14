ec0601f1f066046303ce96229869655a9b31d6a6
Neria13 <silver.eagle@hotmail.de>
2025-09-15 00:27:54 +0200

    Added config.js template

---
+++ config-template.js
@@ -0,0 +1,21 @@
+module.exports = {
+    app: {
+        prefix: "!",
+        token: "insert-bot-token",
+        clientId: "insert-id-of-yourself",
+        guildId: "insert-id-of-your-server",
+        voiceId: "insert-id-of-voice-chat",
+        language: "en",
+        global: true,
+    },
+
+    opt: {
+        DJ: {
+            enabled: false,
+            roleName: 'insert-role-name',
+            commands: []
+        },
+        discordPlayer: {
+        }
+    }
+}
